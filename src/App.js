c2f8033d93c9f903be68355b8e8b7bdc11acd63c
rezahatampoor <rezahtms@gmail.com>
2024-02-06 02:02:29 +0330

    create text expander project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TextExpander />
-    </div>
-  );
-}
-
-const TextExpander = () => {
-  return (
-    <div>
-      <p>
+      <TextExpander
+        collapsedNumWords={100}
+        expandedButtonText="Show Text"
+        collapseButtonText="collapse Text"
+        buttonColor="#ff6622"
+        expanded={false}
+        className="box"
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -19,7 +19,33 @@ const TextExpander = () => {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
-      </p>
+      </TextExpander>
+    </div>
+  );
+}
+
+const TextExpander = ({
+  children,
+  collapsedNumWords = 10,
+  expandedButtonText = "show more",
+  collapseButtonText = "show Less",
+  buttonColor = "#1f09cd",
+  className = "",
+  expanded = false,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+  const handleExpander = () => {
+    setIsExpanded((current) => !current);
+  };
+
+  return (
+    <div className={className}>
+      <span>
+        {isExpanded ? children : children.substr(0, collapsedNumWords) + "..."}
+      </span>
+      <button style={{ color: buttonColor }} onClick={handleExpander}>
+        {isExpanded ? collapseButtonText : expandedButtonText}
+      </button>
     </div>
   );
 };
